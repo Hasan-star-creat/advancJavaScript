4681ed49653d28a0074e9f863344fec7ed9323a2
Hasan-star-creat <74924051+Hasan-star-creat@users.noreply.github.com>
2021-02-04 22:54:05 +0600

    null vs undifined value added

--- logic.js
+++ logic.js
@@ -1,5 +1,8 @@
- const num = {};
-if(num){
+ // Falsy value :
+ // 0 , null , NaN , false , "" , undefine
+ // truthi valu: -1 - 2  to .... , 1+ 2 + ... , [] , {} , " " , ' '
+  let num  = undefined;
+if(num ){
     console.log('this is value is true')
 }
 else{
---
+++ null-vs-undifined.js
@@ -0,0 +1,22 @@
+let sum = 10 ;
+console.log(sum = null);
+
+const arr = [29,20,250,50,60,100];
+ const [first,second, ...friend] = arr;
+ console.log(arr[10]);
+
+const friends = {Name: 'hasan mia', age: 23, hobby: 'coding', }
+const {Name,salary} = friends;
+ console.log(Name,salary);
+ 
+ function doSomething(x, y){
+     
+    console.log(y);
+     return y;
+    
+    }
+    
+   let result = doSomething(32);
+    console.log(result);
+    
+    
